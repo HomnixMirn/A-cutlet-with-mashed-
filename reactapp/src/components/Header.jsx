ae028a756a0d7cf0e82c961ae3dc93279a70e150
Shadow7x <loginvlad1000@gmail.com>
2024-12-07 01:14:43 +0400

    headers , login

--- reactapp/src/components/Header.jsx
+++ reactapp/src/components/Header.jsx
@@ -3,12 +3,23 @@ import { useEffect } from "react";
 import { Link } from 'react-router-dom';
 import event from '../static/img/event.png';
 import region from '../static/img/region.png';
+import axios from "axios";
+import { API_URL } from '../index';
+import { useNavigate } from 'react-router-dom';
 
 
 function Header() {
+    const navigate = useNavigate();
     useEffect(() => {
         document.title = "Header";
     }, []);
+    useEffect(() => {
+        axios.get(API_URL + 'personalInfo', {  headers: {'Authorization': 'Token ' + localStorage.getItem('token')}}).then(res => {
+            const data = res.data
+        }).catch(err => {
+            console.log(err)
+        })
+    })
     return (
         <header>
             <Link className="logo"></Link>
@@ -22,8 +33,15 @@ function Header() {
                     <img src={region}  alt='' className="Region-img"/>
                     <h1 className="regions-h1">Список регионов</h1>
                     </Link>
-                    <Link to="/login" className="personal_info">Вход</Link>
-                    <Link to ="/register" className="register-header">Регистрация</Link>
+                    {localStorage.getItem('token') ?
+                    <div> <Link to="/personalAccount" className="personal_info">Личный кабинет</Link>
+                    <button onClick={() => {localStorage.removeItem('token')
+                    navigate('/')
+                    }} className="register-header">Выход</button>
+                    </div> : 
+                    <div><Link to="/login" className="personal_info">Вход</Link>
+                    <Link to ="/register" className="register-header">Регистрация</Link></div>}
+                    
                 </ul>
             </>
         </header>
