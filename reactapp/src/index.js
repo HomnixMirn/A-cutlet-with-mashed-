f29010b7591e4c260026d880be923d14ac464a29
ZikAsakur <khairutdinovraushan@gmail.com>
2024-12-06 23:59:07 +0300

    PersonalAccountUser

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import Login from './packages/Login';
 import Register from './packages/Register';
 import Map from './packages/MapComponents';
+import PersonalAccountUser from './packages/PersonalAccountUser.jsx';
 import { BrowserRouter, Route, Routes  } from'react-router-dom';
 
 export const API_URL = "http://localhost:8000/api/mininCode/";
@@ -19,6 +20,7 @@ root.render(
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register />} />
           <Route path="/map" element={<Map />} />
+          <Route path = "/PersonalAccountUser" element={<PersonalAccountUser />} />
         </Routes>
     </BrowserRouter>
   // </React.StrictMode>
