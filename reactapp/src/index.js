3932f039b6f6d13c8d13b132e91b352c33974b72
HomnixMirn <vitalik.999f9cfe@gmail.com>
2024-12-07 00:31:25 +0400

    none

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Events from './packages/Events';
 import Login from './packages/Login';
 import Register from './packages/Register';
 import Map from './packages/MapComponents';
@@ -16,6 +17,7 @@ root.render(
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<App/>} />
+          <Route path="/event" element={<Events />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register />} />
           <Route path="/map" element={<Map />} />
