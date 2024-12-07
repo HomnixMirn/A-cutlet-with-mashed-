d307838ea6882971f605862fe6918df6b5a41783
ZikAsakur <khairutdinovraushan@gmail.com>
2024-12-07 12:49:08 +0300

    PerAccUs full css vrode

--- reactapp/src/packages/PersonalAccountUser.jsx
+++ reactapp/src/packages/PersonalAccountUser.jsx
@@ -3,7 +3,7 @@ import {API_URL} from '../index'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import vector_bottom from '../static/img/стрелка.png';
-import Vector from '../static/img/Vector.png';
+import Vector from '../static/img/стрелка2.png';
 import axios from 'axios';
 import './PersonalAccountUser.css'
 
@@ -13,15 +13,6 @@ export default function PersonalAccountUser() {
 
     const [gender, setGender] = useState('');
     const [isGenderDropdownOpen, setIsGenderDropdownOpen] = useState(false);
-
-    const [country, setCountry] = useState('');
-    const [isCountryDropdownOpen, setIsCountryDropdownOpen] = useState(false);
-
-    const [region, setRegion] = useState('');
-    const [isRegionDropdownOpen, setIsRegionDropdownOpen] = useState(false);
-
-    const [city, setCity] = useState('');
-    const [isCityDropdownOpen, setIsCityDropdownOpen] = useState(false);
 
     const toggleDropdown = (setDropdownOpen) => {
         setDropdownOpen(prevState => !prevState);
@@ -80,30 +71,37 @@ export default function PersonalAccountUser() {
                     <div className="LocalData_PerAccUs">
                         <div className="LocalInfo_PerAccUs">
                             <h1 className="h1_PerAccUs">Личные данные</h1>
-                            <p className="p_PerAccUs">Данные, которые будут использоваться для идентефицирования личности и подготовки сертификатов</p>
+                            <p className="p_PerAccUs">данные, которые будут использоваться для идентефицирования личности и подготовки сертификатов</p>
                         </div>
                         <div className="LocalInfoInputs_PerAccUs">
                             <input type="text" className="LocalDataInputFIO_PerAccUs" placeholder='Введите ваше ФИО'/> 
 
                             <div className="DivLocalInfoPhoneBirthday_PerAccUs">
-                                <input type="text" className="LocalDataInputPhoneBirthday_PerAccUs" placeholder='Введите ваш номер телефона' /> 
-                                <input type="text" className="LocalDataInputPhoneBirthday_PerAccUs" placeholder='Введите ваш день рождения' /> 
+                                <input type="phone" className="LocalDataInputPhoneBirthday_PerAccUs" placeholder='Введите ваш номер телефона' /> 
+                                <input type="date" className="LocalDataInputPhoneBirthday_PerAccUs" placeholder='Введите ваш день рождения' /> 
                             </div>
 
                             <div className="LocalInfoGender_PerAccUs">
                                 <div className="dropdown-header">
-                                    {gender || 'Ваш пол'}
-                                    {isGenderDropdownOpen 
-                                        ? <img src = {Vector} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsGenderDropdownOpen)} />
-                                        : <img src = {vector_bottom} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsGenderDropdownOpen)} />
-                                    }
+                                    <div className="dropdown_PerAccUs">
+                                        <p className="PGender_PerAccUs">{gender || 'Ваш пол'}</p>
+                                        {isGenderDropdownOpen 
+                                            ? <img src = {Vector} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsGenderDropdownOpen)} />
+                                            : <img src = {vector_bottom} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsGenderDropdownOpen)} />
+                                        }
+                                    </div>
+                                    {isGenderDropdownOpen &&(
+                                        <ul className="dropdown-list">
+                                            <div className='DivDropDownMale_PerAccUs'>
+                                                <li className="DropDownMale_PerAccUs" onClick = {() => selectOption(setGender, setIsGenderDropdownOpen,'Мужской')}>Мужской</li>
+                                            </div>
+                                            <div className='DropDownBorder_PerAccUs'></div>
+                                            <div className="DivDropDownFemale_PerAccUs">
+                                                <li className="DropDownFemale_PerAccUs" onClick = {() => selectOption(setGender, setIsGenderDropdownOpen,'Женский')}>Женский</li>
+                                            </div>
+                                        </ul>
+                                    )}
                                 </div>
-                                {isGenderDropdownOpen &&(
-                                    <ul className="dropdown-list">
-                                        <li onClick = {() => selectOption(setGender, setIsGenderDropdownOpen,'Мужской')}>Мужской</li>
-                                        <li onClick = {() => selectOption(setGender, setIsGenderDropdownOpen,'Женский')}>Женский</li>
-                                    </ul>
-                                )}
                                 <input type="text" className="LocalInfoEmail_PerAccUs" value ={email} readOnly placeholder ="Ваша почта"/>
                             </div>
                         </div>  
@@ -113,77 +111,34 @@ export default function PersonalAccountUser() {
                 <div className="GeneralMainLocation_PerAccUs">
                     <div className="MainLocation_PerAccUs">
                         <div className="LocationInfo_PerAccUs">
-                            <h1 className="h1_PerAccUs">местоположение</h1>
-                            <p className="p_PerAccUs">Местоположение, которое будет использоваться для идентификации личности и подготовки сертификатов</p>
+                            <h1 className="h1_PerAccUs">Местоположение</h1>
+                            <p className="p_PerAccUs">местоположение, которое будет использоваться для идентификации личности и подготовки сертификатов</p>
                         </div>
 
                         <div className="Location_PerAccUs">
-                            <div className="LocationCountry_PerAccUs">
-                                {country || 'Выберите страну'}
-                                {isCountryDropdownOpen 
-                                        ? <img src = {Vector} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsCountryDropdownOpen)} />
-                                        : <img src = {vector_bottom} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsCountryDropdownOpen)} />
-                                    }
+                            <div className="LocationInputs_PerAccUs">
+                                <input type="text" className="LocationInputContry_PerAccUs" placeholder='Введите вашу страну' />
+                                <input type="text" className="LocationInputRegion_PerAccUs" placeholder='Введите ваш регион' />
+                                <input type="text" className="LocationInputCity_PerAccUs" placeholder='Введите ваш город' />
                             </div>
-                            {isCountryDropdownOpen &&(
-                                <input 
-                                    type="text" 
-                                    className="Input_PerAccUs"
-                                    value = {country}
-                                    onChange ={(e) => handleInputChange(setCountry,setIsCountryDropdownOpen, e.target.value)}
-                                    placeholder="Введите или выберите страну" 
-                                />
-                            )}
-
-                            <div className="LocationRegion_PerAccUs">
-                                {region || 'Выберите регион' }
-                                {isRegionDropdownOpen 
-                                        ? <img src = {Vector} className = "dropdown-arrow" onClick = {() => toggleDropdown( setIsRegionDropdownOpen)} />
-                                        : <img src = {vector_bottom} className = "dropdown-arrow" onClick = {() => toggleDropdown( setIsRegionDropdownOpen)} />
-                                    }
-                            </div>
-                            {isRegionDropdownOpen &&(
-                                <input 
-                                    type="text" 
-                                    className="Input_PerAccUs"
-                                    onChange={(e) => handleInputChange(setRegion, setIsRegionDropdownOpen,e.target.value)}
-                                    placeholder="Введите или выберите регион"
-                                /> 
-                            )}
-
-                                <div className="LocationCity_PerAccUs">
-                                    {city || 'Выберите город'}
-                                    {isCityDropdownOpen 
-                                            ? <img src = {Vector} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsCityDropdownOpen)} />
-                                            : <img src = {vector_bottom} className = "dropdown-arrow" onClick = {() => toggleDropdown(setIsCityDropdownOpen)} />
-                                        }
-                                </div>
-                                {isCityDropdownOpen &&(
-                                    <input 
-                                        type="text" 
-                                        className="Input_PerAccUs"
-                                        onChange={(e) => handleInputChange(setCity,setIsCityDropdownOpen, e.target.value)} 
-                                        placeholder="Введите или выберите город"
-                                    />
-                                )}
-                
                         </div>
                     </div>
                 </div>
 
-                <div className="ChangePassword_PerAccUs">
-                    <div className="ChangePasswordInfo_PerAccUs">
-                        <h1 className="h1_PerAccUs">Смена пароля</h1>
-                        <p className="p_PerAccUs">При нажатии на кнопку смены пароля,вам на почту придет ссылка для подтвеждения и выбора нового пароля</p>
-                    </div>
-                    <div className="DivChange_PerrAccUs">
-                        <input type="text" className="type" />Ваш пароль
-                        <button className="ButtChange_PerrAccUs">Сменить пароль</button>
-                    </div>
+                <div className="GeneralChangePassword_PerAccUs">
+                    <div className='ChangePassword_PerAccUs'>
+                        <div className="ChangePasswordInfo_PerAccUs">
+                            <h1 className="h1_PerAccUs">Смена пароля</h1>
+                            <p className="p_PerAccUs">При нажатии на кнопку смены пароля,вам на почту придет ссылка для подтвеждения и выбора нового пароля</p>
+                        </div>
+                        <div className="DivChange_PerrAccUs">
+                            <button className="ButtChange_PerrAccUs">Сменить пароль</button>
+                        </div>
+                    </div>    
                 </div>
 
                 <div className="DivSave_PerAccUs">
-                    <button className="ButtSave_PerAccUs">Сохранить данные</button>
+                    <button className="ButtSave_PerAccUs">СОХРАНИТЬ ДАННЫЕ</button>
                 </div>
             </div>
         </div>
