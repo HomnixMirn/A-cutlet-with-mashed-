1b0df5f9619a92fa6a85d6102697b075cbf04271
Shadow7x <loginvlad1000@gmail.com>
2024-12-07 17:37:19 +0400

    fixx

--- reactapp/src/packages/Events.jsx
+++ reactapp/src/packages/Events.jsx
@@ -67,8 +67,7 @@ function Events() {
         <div className='wrapper'>
             <Header />
             <h2></h2>
-            
->>>>>>> 1e7e1bd22669750c67ca11fbeb7d3d9d1bd54f55
+        
             <div className="search_input">
             <input type="text" placeholder="Поиск"  className='search' value={search} onChange={(e) => setSearch(e.target.value)}/>
             <div><img src={loopa} alt=""  className="loopa"/></div>
